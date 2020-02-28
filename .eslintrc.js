6046bee743ae2eced3c20348f82d0ddac9171858
黒木一刀 <Kuroki2033550>
2020-02-28 18:15:58 +0800

    初始化项目

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    // 强制语句结束添加，分号
+    semi: ['error', 'always'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-}
+};
